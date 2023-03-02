126c2eb4c84d886464b707cc02ccee69304e81d4
Daeki <kimdaeki@gmail.com>
2023-03-02 12:51:59 +0900

    Interceptor1

--- src/main/java/com/iu/s1/interceptors/MemberCheckInterceptor.java
+++ src/main/java/com/iu/s1/interceptors/MemberCheckInterceptor.java
@@ -1,11 +1,13 @@
 package com.iu.s1.interceptors;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
 @Component
 public class MemberCheckInterceptor extends HandlerInterceptorAdapter {
 	
@@ -16,12 +18,27 @@ public class MemberCheckInterceptor extends HandlerInterceptorAdapter {
 		//Controller 진입전
 		// return이 true라면 다음 Controller로 진행
 		// return이 true라면 다음 Controller로 진행 X
-		System.out.println("Controller 진입 전 체크");
-		response.sendRedirect("");
-		return true;
+		System.out.println("Member Check Interceptor");
+		Object obj = request.getSession().getAttribute("member");
+		
+		if(obj != null) {
+			return true;
+		}
+		System.out.println("로그인 안한 경우");
+		//1. Foward Jsp
+//		request.setAttribute("result", "권한이 없습니다");
+//		request.setAttribute("url", "../member/memberLogin");
+//		RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/common/result.jsp");
+//		view.forward(request, response);
+		
+		//2. Redirect
+		response.sendRedirect("../../../../../member/memberLogin");
+		
+		
+		return false;
 	}
 	
-
+	
 	
 	
 
